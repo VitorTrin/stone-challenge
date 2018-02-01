5768907e1e13b355dce86827ffd3c7e88cc0bc19
Vítor <emailreservadovitor@gmail.com>
2018-02-01 11:06:16 -0200

    New functions for money checking: is_money/1 and same_currency/2

--- lib/financial_system.ex
+++ lib/financial_system.ex
@@ -23,7 +23,32 @@ defmodule FinancialSystem do
   defmodule Money do
     defstruct integer: 0, frac: 0, iso: %ISO{}
   end
-  
+
+  def is_money(suspect) do
+    case{is_map(suspect),
+      Map.has_key?(suspect, :integer),
+      Map.has_key?(suspect, :frac),
+      Map.has_key?(suspect, :iso)}
+    do
+      {:true, :true, :true, true} -> :true
+      _ -> :false
+    end
+  end
+
+  def same_currency(first, second) do
+    case {check_if_is_money(first),
+      check_if_is_money(second),
+      first.iso == second.iso}
+    do
+      {:true, :true, :true} ->[:ok]
+      {:true, :true, :false} ->[:err, "Not same currency"]
+      {:false, :false, _} -> [:err, "Parameters aren't currency"]
+      {:false, _, _} -> [:err, "Parameter 1 isn't currency"]
+      {_, :false, _} -> [:err, "Parameter 2 isn't currency"]
+      _ -> [:err]
+    end
+  end
+
   # def sum (first, second) do
   #   if first do
   #
