69db110f794c10da5452627f01d8bb37794fdacd
Vítor <emailreservadovitor@gmail.com>
2018-02-08 17:40:49 -0200

    new function transfer/3. Not complete yet due to sub function being a dummy

--- lib/financial_system.ex
+++ lib/financial_system.ex
@@ -17,11 +17,27 @@ defmodule FinancialSystem do
   end
 
   defmodule ISO do
-    defstruct code: "BRL", number: 986, exponent: 2
+    @moduledoc """
+    Provides a struct that stores ISO 4217 information.
+    More info at https://en.wikipedia.org/wiki/ISO_4217
+    """
+    defstruct alpha_code: "BRL", numeric_code: 986, exponent: 2
   end
 
   defmodule Money do
+    @moduledoc """
+    Provides a struct that stores currency amounts.
+
+    The int field stores the integer part of the number, and the frac stores the
+    fractional part of the number.
+
+    When the value is negative, both int and frac store are negative.
+    """
     defstruct int: 0, frac: 0, iso: %ISO{}
+  end
+
+  defmodule Account do
+    defstruct user: 0, balance: [%Money{}]
   end
 
   @doc """
@@ -45,17 +61,29 @@ defmodule FinancialSystem do
   @doc """
     Tests if 2 maps use the same currency by checking their iso field.
   """
+  # @spec same_currency(Money, Money) :: list(atom)
+  # def same_currency(first, second) do
+  #   case {is_money(first),
+  #     is_money(second),
+  #     first.iso == second.iso}
+  #   do
+  #     {:true, :true, :true} ->[:ok]
+  #     {:true, :true, :false} ->[:err, "Not same currency"]
+  #     {:false, :false, _} -> [:err, "Parameters aren't currency"]
+  #     {:false, _, _} -> [:err, "Parameter 1 isn't currency"]
+  #     {_, :false, _} -> [:err, "Parameter 2 isn't currency"]
+  #     _ -> [:err]
+  #   end
+  # end
+
+  @spec same_currency(Money, Money) :: boolean
   def same_currency(first, second) do
     case {is_money(first),
       is_money(second),
       first.iso == second.iso}
     do
-      {:true, :true, :true} ->[:ok]
-      {:true, :true, :false} ->[:err, "Not same currency"]
-      {:false, :false, _} -> [:err, "Parameters aren't currency"]
-      {:false, _, _} -> [:err, "Parameter 1 isn't currency"]
-      {_, :false, _} -> [:err, "Parameter 2 isn't currency"]
-      _ -> [:err]
+      {:true, :true, :true} -> true
+      _ -> false
     end
   end
 
@@ -94,4 +122,59 @@ defmodule FinancialSystem do
       guard
     end
   end
+
+  def sub(first, second) do
+    #dummy
+  end
+
+  def compare(first, second) do
+    guard = same_currency(first, second)
+    if guard do
+      cond do
+        first.int > second.int ->
+          1
+        first.int == second.int && first.frac > second.frac ->
+          1
+        first.int == second.int && first.frac == second.frac ->
+          0
+        true ->
+          -1
+      end
+    else
+      guard
+    end
+  end
+
+  def transfer(source, destination, amount) do
+    #check if source has that amount
+    [result | _] = Enum.filter(source.balance, fn(x) -> (same_currency(x, amount)) end)
+    source = if(compare(result, amount) >= 0) do
+    #updating the source
+     %{source | balance: Enum.map(source.balance, fn(x) ->
+        if x == result do
+          sub(result, amount)
+        else
+          x
+        end end)}
+      else
+        [:err]
+      end
+
+    #updating or creating the destination
+    [result | _] = Enum.filter(destination.balance, fn(x) -> (same_currency(x, amount)) end)
+    destination =  cond do
+      #if source returned an error, destination should not be an error too
+      source == [:err] -> [:err]
+      #case there is no such currency on the destination, we create it
+      result == nil -> %{destination | balance: [amount | destination.balance]}
+      true -> %{destination | balance: Enum.map(destination.balance, fn(x) ->
+        if(x == result) do
+          sum(result, amount)
+        else
+          x
+        end end)}
+    end
+    [source, destination]
+  end
+
 end
